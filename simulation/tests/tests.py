685141cfceb289bf4aad373de50549981b28d888
Ben Latham <bl15686@my.bristol.ac.uk>
2017-07-08 18:07:14 +0100

    updated tests

--- simulation/tests/tests.py
+++ simulation/tests/tests.py
@@ -1,12 +1,23 @@
 import unittest
-import weathergen
+from unittest import TestCase
+from simulation.dairy import intake
+from simulation.loadFeeds import types_file
+from csvReader.csvReader import read_contents
 
-class PrimesTestCase(unittest.TestCase):
+
+class FeedLoaderTestCase(TestCase):
+    def test_types_file(self):
+        contents ="grain,,0.9\ngrass,0.5,\nfat,0.1,0.2"
+        processed = read_contents(contents,types_file())
+        self.assertEquals(processed[0][0], "grain")
+        self.assertEquals(processed[1][0], "grass")
+        self.assertEquals(processed[2][0], "fat")
 
 
-    def test_is_five_prime(self):
-        """Is five successfully determined to be prime?"""
-        self.assertTrue(is_prime(5))
+
+class IntakeTestCase(TestCase):
+    def test_intake_with_concentrate(self):
+        self.assertAlmostEqual(intake(0.45, 450.0, 1.0, 26.0), 13.84,2)
 
 if __name__ == '__main__':
     unittest.main()
