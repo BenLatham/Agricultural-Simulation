268291968c3efd0bb05ba5678ed7ac3192787edd
Ben Latham <bl15686@my.bristol.ac.uk>
2017-08-07 12:28:43 +0100

    test admin.py

--- simulation/tests/tests.py
+++ simulation/tests/tests.py
@@ -1,9 +1,10 @@
-import unittest
-from unittest import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase
 from simulation.dairy import intake
 from simulation.loadFeeds import types_file
 from csvReader.csvReader import read_contents
-from simulation.admin import load_goods
+from simulation.admin import Admin
+import simulation.models as models
 
 
 class FeedLoaderTestCase(TestCase):
@@ -15,13 +16,19 @@ class FeedLoaderTestCase(TestCase):
         self.assertEquals(processed[2][0], "fat")
 
 class AdminTestCase(TestCase):
+    def setUp(self):
+        user = User()
+        user.save()
+        scenario = models.Scenario(user=user)
+        scenario.save()
+        self.admin =Admin(scenario)
+        self.admin.setup()
+
     def test_quick_test(self):
-        self.assertEquals(load_goods(),{"bale","straws","kg","l"})
+        self.assertEquals(True,True)
 
 
 class IntakeTestCase(TestCase):
     def test_intake_with_concentrate(self):
         self.assertAlmostEqual(intake(0.45, 450.0, 1.0, 26.0), 13.84,2)
 
-if __name__ == '__main__':
-    unittest.main()
